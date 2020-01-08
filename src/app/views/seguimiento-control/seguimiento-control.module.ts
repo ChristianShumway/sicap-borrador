ef7fcafbdaa7440bebc5bda3990971ff0617f477
Christian Shumway <christianshumway@Christians-MacBook-Pro.local>
2020-01-07 21:12:55 -0600

    se crea modulo seguimiento y control

---
+++ src/app/views/seguimiento-control/seguimiento-control.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SeguimientoControlRoutingModule } from './seguimiento-control-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SeguimientoControlRoutingModule
+  ]
+})
+export class SeguimientoControlModule { }
